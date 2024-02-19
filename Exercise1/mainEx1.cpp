30e70121a456b26e0a349184ec0b8d49dc272f31
nectifurkn455 <99787950+nectifurkn455@users.noreply.github.com>
2024-02-19 13:56:36 +0100

    Exercise created and uploaded.

---
+++ Exercise1/mainEx1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name;
+    int option;
+    string arr_options[] = {"Sortir", "Benvinguda"};
+
+    cout << "Hola, com et dius";
+    cin >> name;
+
+
+ }
